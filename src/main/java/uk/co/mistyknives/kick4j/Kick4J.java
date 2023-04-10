e5a65ab632601d9e6368a71df9278852558b8cb7
MistyKnives <misty@mistyknives.co.uk>
2023-04-10 20:05:31 +0100

    Added Multiple Methods to Kick4J Main Class
    Removed the testing method Kick4J#main and added multiple methods to make accessing the methods easier.
    
    Added the following:
     - Kick4J#getUser
     - Kick4J#getUsers
     - Kick4J#getStreamer
     - Kick4J#getStreamers
     - Kick4J#getStream
     - Kick4J#getStreams
     - Kick4J#getChatroom
     - Kick4J#getChatrooms
    
    you can access all these methods by either creating a new class instance of Kick4J e.g.
    
    - private Kick4J api = new Kick4J();
    
    OR
    
    - Kick4J.getAPI().Method

--- src/main/java/uk/co/mistyknives/kick4j/Kick4J.java
+++ src/main/java/uk/co/mistyknives/kick4j/Kick4J.java
@@ -1,22 +1,54 @@
 package uk.co.mistyknives.kick4j;
+
+import uk.co.mistyknives.kick4j.chatroom.Chatroom;
 import uk.co.mistyknives.kick4j.chatroom.ChatroomAPI;
+import uk.co.mistyknives.kick4j.chatroom.ChatroomList;
+import uk.co.mistyknives.kick4j.stream.Stream;
 import uk.co.mistyknives.kick4j.stream.StreamAPI;
-import uk.co.mistyknives.kick4j.user.UserAPI;
+import uk.co.mistyknives.kick4j.stream.StreamList;
+import uk.co.mistyknives.kick4j.streamer.Streamer;
 import uk.co.mistyknives.kick4j.streamer.StreamerAPI;
+import uk.co.mistyknives.kick4j.streamer.StreamerList;
+import uk.co.mistyknives.kick4j.user.User;
+import uk.co.mistyknives.kick4j.user.UserAPI;
+import uk.co.mistyknives.kick4j.user.UserList;
 
 /**
  * Copyright MistyKnives © 2022-2023
  */
 public class Kick4J {
 
-    public static void main(String[] args) {
-        System.out.println("---------- stream api ----------");
-        System.out.println(new StreamAPI().getStream("Loochy"));
-        System.out.println("---------- streamer api ----------");
-        System.out.println(new StreamerAPI().getStreamer("Loochy"));
-        System.out.println("---------- chatroom api ----------");
-        System.out.println(new ChatroomAPI().getChatroom("Loochy"));
-        System.out.println("---------- user api ----------");
-        System.out.println(new UserAPI().getUser("Loochy"));
+    private static final Kick4J api = new Kick4J();
+
+    public static Kick4J getAPI() {
+        return api;
+    }
+    
+    public User getUser(String s) {
+        return new UserAPI().getUser(s);
+    }
+    public UserList getUsers(String... s) {
+        return new UserAPI().getUsers(s);
+    }
+
+    public Streamer getStreamer(String s) {
+        return new StreamerAPI().getStreamer(s);
+    }
+    public StreamerList getStreamers(String... s) {
+        return new StreamerAPI().getStreamers(s);
+    }
+
+    public Stream getStream(String s) {
+        return new StreamAPI().getStream(s);
+    }
+    public StreamList getStreams(String... s) {
+        return new StreamAPI().getStreams(s);
+    }
+
+    public Chatroom getChatroom(String s) {
+        return new ChatroomAPI().getChatroom(s);
+    }
+    public ChatroomList getChatrooms(String... s) {
+        return new ChatroomAPI().getChatrooms(s);
     }
 }
